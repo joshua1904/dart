214a2bceeb841c8a1f1021503c7eae0785d297b2
Joshua Tewes <joshua.tewes@check24.de>
2025-09-20 17:21:08 +0200

    frontend

--- dart/main/views/single_player/game.py
+++ dart/main/views/single_player/game.py
@@ -9,11 +9,16 @@ from main.models import Game
 class GameView(views.View):
     def get(self, request, game_id: int):
         game = get_object_or_404(Game, pk=game_id)
+        if request.user != game.player:
+            return redirect(reverse_lazy('home'))
         return render(request, 'single_player/game.html', context=get_game_context(game))
 
     def post(self, request, game_id):
+
         points = int(request.POST.get('points', 0))
         game = get_object_or_404(Game, id=game_id)
+        if request.user != game.player:
+            return redirect(reverse_lazy('home'))
         game_status = add_round(game=game, points=points)
         if game_status == 1:
             return redirect(reverse_lazy('result', kwargs={'game_id': game.id}))
