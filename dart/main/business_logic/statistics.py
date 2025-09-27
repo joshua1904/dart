714c20fd3b984453d9d6f276f0ca39dd6b48b23e
MenschMeier <joshua.t@outlook.de>
2025-09-28 01:55:10 +0200

    style black

--- dart/main/business_logic/statistics.py
+++ dart/main/business_logic/statistics.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from dart.main.models import Game
+from main.models import Game
 
 
 @dataclass
@@ -9,17 +9,19 @@ class Statistics:
     losses: int = 0
     total_games: int = 0
     total_points: int = 0
-    
+
     @property
     def win_rate(self) -> float:
         return self.wins / self.total_games
-    
+
     @property
     def loss_rate(self) -> float:
         return self.losses / self.total_games
-    
+
     @property
     def average_points(self) -> float:
         return self.total_points / self.total_games
-    
+
+
 def get_statistics(games: list[Game]) -> Statistics:
+    pass
